52e84a3b5e38a335f4fa465bfbdc5ee61d98da4c
budhil <budhilnigam@gmail.com>
2024-07-15 23:59:36 +0530

    Made a test route to check if server is running good

--- backend/main.py
+++ backend/main.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 CORS(app)
 SQLAlchemy(app)
 
+@app.route('/api', methods=['GET'])
+def get():
+    return jsonify({'msg': 'Hello World'})
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 if __name__ == '__main__':
     app.run(debug=True)
