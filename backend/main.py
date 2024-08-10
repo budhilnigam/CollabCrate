7c41191fb01c6a9041de41340368e54235207f10
budhil <budhilnigam@gmail.com>
2024-08-10 17:12:45 +0530

    Created register, logout and ad request function

--- backend/main.py
+++ backend/main.py
@@ -1,16 +1,21 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import login_user,login_required,LoginManager,logout_user
+from flask_login import login_user,login_required,LoginManager,logout_user,current_user
 from celery import Celery
 from flask_bcrypt import check_password_hash, generate_password_hash, Bcrypt
 from models import *
+import os
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SECRET_KEY'] = 'mysecretkey'
+app.config['SQLALCHEMY_DATABASE_URI'] =\
+    'sqlite:///'+ os.path.join(basedir, 'database.db')
 app.config['CACHE_TYPE'] = 'redis'
 app.config['CACHE_REDIS_URL'] = 'redis://localhost:6379/0'
 app.config['CELERY_BROKER_URL'] = 'redis://localhost:6379/1'
 app.config['CELERY_RESULT_BACKEND'] = 'redis://localhost:6379/1'
+
 cors=CORS(app)
 bcrypt=Bcrypt(app)
 db.init_app(app)
@@ -22,11 +27,11 @@ def load_user(user):
     user_type, user_name = user.split(":")
     
     if user_type == 'admin':
-        return Admin.query.get(user_name)
+        return Admin.query.filter_by(username=user_name).first()
     elif user_type == 'sponsor':
-        return Sponsor.query.get(user_name)
+        return sponsors.query.filter_by(username=user_name).first()
     elif user_type == 'influencer':
-        return Influencer.query.get(user_name)
+        return Influencer.query.filter_by(username=user_name).first()
     return None
 
 @app.route('/api', methods=['GET'])
@@ -36,6 +41,32 @@ def get():
 @app.route('/')
 def index():
     return 'Hello, World!'
+
+@app.route('/register', methods=['POST'])
+def register():
+    username = request.form['username']
+    password = request.form['password']
+    role = request.form['role']
+
+    if role == 'sponsor':
+        if sponsors.query.filter_by(username=username).first():
+            return {"message":"Another user already exists with this username"}
+        sp_industry=request.form['sp_industry']
+        sp_budget=request.form['sp_budget']
+        sponsor = sponsors(username=username, password=bcrypt.generate_password_hash(password),sp_industry=sp_industry,sp_budget=sp_budget)
+        db.session.add(sponsor)
+        db.session.commit()
+        return {"message":True}
+    elif role == 'influencer':
+        if Influencer.query.filter_by(username=username).first():
+            return {"message":"Another user already exists with this username"}
+        inf_category=request.form['inf_category']
+        inf_niche=request.form['inf_niche']
+        inf_reach=request.form['inf_reach']
+        influencer = Influencer(username=username, password=bcrypt.generate_password_hash(password),inf_category=inf_category,inf_niche=inf_niche,inf_reach=inf_reach)
+        db.session.add(influencer)
+        db.session.commit()
+        return {"message":True}
 
 @app.route('/login', methods=['POST'])
 def login():
@@ -47,15 +78,42 @@ def login():
         if role == 'admin':
             user = Admin.query.filter_by(username=username).first()
         elif role == 'sponsor':
-            user = Sponsor.query.filter_by(username=username).first()
+            user = sponsors.query.filter_by(username=username).first()
         elif role == 'influencer':
             user = Influencer.query.filter_by(username=username).first()
 
         if user and bcrypt.check_password_hash(user.password, password):
-            login_user(user, remember=True, id=f'{role}:{user.username}')
+            login_user(user)
             return {"message":True}
         else:
             return {"message":False}
+        
+
+@app.route('/logout', methods=['POST'])
+@login_required
+def logout():
+    logout_user()
+    return {"message":True}
+
+@app.route('/ad_requests', methods=['GET','POST'])
+@login_required
+def ad_requests():
+    if request.method == 'GET':
+        if current_user.get_id().split(':')[0] == 'admin' or current_user.get_id().split(':')[0] == 'sponsor':
+            sponsor_username=request.args.get('sponsor_username')
+            if request.args.get('which')=='all':
+                return jsonify(db.session.query(AdRequest).join(Campaign, AdRequest.cmpn_id==Campaign.cmpn_id).join(sponsors, Campaign.sp_id==sponsors.sp_id).filter(sponsors.username==sponsor_username).all())
+            else:
+                return jsonify(AdRequest.query.filter_by(id=current_user.id, status=request.args.get('which')).all())
+    elif request.method == 'POST':
+        cmpn_id = request.form['cmpn_id']
+        inf_id = request.form['inf_id']
+        message = request.form['message']
+        payment_amt = request.form['payment_amt']
+        ad_request = AdRequest(cmpn_id=cmpn_id, inf_id=inf_id, message=message, payment_amt=payment_amt)
+        db.session.add(ad_request)
+        db.session.commit()
+        return {"message":True}
 
 if __name__ == '__main__':
     app.run(debug=True)
