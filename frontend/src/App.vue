c265b892d04f979a3d7e5eb26033c27b3a61193a
budhil <budhilnigam@gmail.com>
2024-09-24 23:27:29 +0530

    Commented out Router Links for now

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -7,8 +7,8 @@ import HelloWorld from './components/HelloWorld.vue'
   <header>
     <div class="wrapper">
       <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+        <!--RouterLink to="/">Home</RouterLink-->
+        <!--RouterLink to="/about">About</RouterLink-->
       </nav>
     </div>
   </header>
