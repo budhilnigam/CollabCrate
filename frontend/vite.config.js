b8f6ca3037fab62ff757981ff22745efe75b6560
budhil <budhilnigam@gmail.com>
2024-12-01 23:55:19 +0530

    'server' proxy for flask api server

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -17,4 +17,15 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+  server:{
+    proxy: {
+      // with options
+      "/server": {
+        target: "http://127.0.0.1:5000",
+        changeOrigin: true,
+        secure: false,
+        rewrite: path => path.replace(/^\/server/, '')
+      }
+    }
+  }
 })
